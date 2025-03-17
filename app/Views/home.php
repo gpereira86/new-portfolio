1696995f3b1d9fd44e210ab4abee8bc09dd3ded6
Glauco Pereira <glaucoadm@gmail.com>
2025-03-17 16:41:13 -0300

    Página Home

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,6 +4,13 @@
 <?php echo $this->section('content') ?>
 
 <?php echo $this->renderSection('nav') ?>
+
+<ul>
+    <li>Teste</li>
+    <li>Teste</li>
+    <li>Teste</li>
+</ul>
+
 <h1>teste de cores 1</h1>
 <h2>teste de cores 2</h2>
 <h3>teste de cores 3</h3>
